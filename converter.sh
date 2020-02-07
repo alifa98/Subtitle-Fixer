159dc21bbb37e75da5d6dbac10303c2e4208a689
Justice <Contact@alifaraji.ir>
2020-02-07 18:50:31 +0330

    initial commit

---
+++ converter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+#    _    _ _   _____                _ _ 
+#   / \  | (_) |  ___|_ _ _ __ __ _ (_|_)
+#  / _ \ | | | | |_ / _` | '__/ _` || | |
+# / ___ \| | | |  _| (_| | | | (_| || | |
+#/_/   \_\_|_| |_|  \__,_|_|  \__,_|/ |_|
+#                                 |__/   
+#
+#	by Ali faraji, Feb 2019
+#	Subtitle Encoding Fixer.   (^_^)
+
+
+
+TO_ENCODING="UTF-8"
+if [ ! -f "converted" ]; then
+    mkdir converted
+fi
+for  file  in  *.srt; do
+	FROM_ENCODING=`uchardet "$file" `
+	CONVERT=" iconv  -f   $FROM_ENCODING  -t   $TO_ENCODING"
+     	$CONVERT   "$file"   -o  "./converted/${file}[utf8].srt"
+done
+exit 0
